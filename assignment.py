4a3e76b163fa365db8130a17063bcf6da5e98ada
ssm-0123 <90653235+ssm-0123@users.noreply.github.com>
2022-11-04 11:36:02 -0700

    ss

--- assignment.py
+++ assignment.py
@@ -22,3 +22,33 @@ retrieve a record by phone number and display all of the information
 You will need to create the table yourself. Consider what data types you will
 need to use.
 """
+
+import sqlite3
+
+file = 'dbdb.db'
+connection = sqlite3.connect(file)
+
+cursor = connection.cursor()
+
+Tempscript = """
+DROP TABLE IF EXISTS Customers;
+
+CREATE TABLE Customers(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+petname TEXT,
+petspecies TEXT, 
+pet breed TEXT,
+department TEXT,
+position TEXT,
+hireDate TEXT);
+
+INSERT INTO Customers(name, salary, department, position, hireDate) VALUES('Dave', 300, 'Marketing', 'LV1', '2020-01-01');
+INSERT INTO Customers(name, salary, department, position, hireDate) VALUES('Clara', 420, 'Sales', 'LV2', '2018-01-11');
+INSERT INTO Customers(id, name, salary, department, position, hireDate) VALUES(3, 'Jane', 620, 'Developer', 'LV4', '2015-11-01');
+INSERT INTO Customers VALUES(4, 'Peter', 530, 'Developer', 'LV2', '2020-11-01'); 
+"""
+
+
+
+cursor.execute('CREATE Customers; pet name, pet species, pet breed, owner name, owner phone number, owner email, owner balance, date of first visit')
+
